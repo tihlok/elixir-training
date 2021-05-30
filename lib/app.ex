7af00bf909cd67e9e029340161b2629791073872
Tiago Couto <strife.tiago@gmail.com>
2021-05-30 14:42:53 +0000

    starting project

---
+++ lib/app.ex
@@ -0,0 +1,18 @@
+defmodule Leaderboard do
+  @moduledoc """
+  Documentation for `Leaderboard`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Leaderboard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/app_test.exs
@@ -0,0 +1,8 @@
+defmodule LeaderboardTest do
+  use ExUnit.Case
+  doctest Leaderboard
+
+  test "greets the world" do
+    assert Leaderboard.hello() == :world
+  end
+end
